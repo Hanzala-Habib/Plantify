685f29664dfc05988e7ed504094af1e19d0f9f0e
Hanzala Habib <imhanzala444@gmail.com>
2025-07-26 10:10:48 +0500

    recomended plants part completed

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:plantify/constants/colors.dart';
 
+import '../constants/tile.dart';
+
 class Homescreen extends StatelessWidget {
   const Homescreen({super.key});
 
@@ -19,7 +21,7 @@ class Homescreen extends StatelessWidget {
                     bottomRight: Radius.circular(40),
                   ),
                   child: Container(
-                    height: 250,
+                    height: 200,
                     width: double.maxFinite,
                     color: mainColor,
                   ),
@@ -30,7 +32,7 @@ class Homescreen extends StatelessWidget {
                 ),
 
                 Padding(
-                  padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+                  padding: const EdgeInsets.only(top: 80, left: 20, right: 20),
                   child: Row(
                     children: [
                       Text(
@@ -50,7 +52,7 @@ class Homescreen extends StatelessWidget {
 
                 Padding(
                   padding: const EdgeInsets.only(
-                    top: 220.0,
+                    top: 175.0,
                     left: 20,
                     right: 20,
                   ),
@@ -130,60 +132,66 @@ class Homescreen extends StatelessWidget {
 
             SizedBox(height: 20),
             Container(
-              height: 280,
-              width: 180,
+              height: 230,
+              width: double.maxFinite,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Tile(
+                    image: 'assets/images/image_1.png',
+                    country: "Russia",
+                    Name: "SAMANTHA",
+                    Price: "\$400",
+                  ),
+                  SizedBox(width: 10),
+                  Tile(
+                    image: 'assets/images/image_2.png',
+                    country: "Russia",
+                    Name: "LEVITA",
+                    Price: "\$200",
+                  ),
 
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white,
-                boxShadow: [BoxShadow(
-                    color: mainColor,
-                    offset: Offset(0, 10),
-                    blurRadius: 20
-                )
+                  SizedBox(width: 10),
+                  Tile(
+                    image: 'assets/images/image_3.png',
+                    country: "Russia",
+                    Name: "Alovera",
+                    Price: "\$100",
+                  ),
                 ],
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20, top: 25),
+              child: Row(
                 children: [
+                  Text(
+                    "Featured Plants",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  Spacer(),
                   Container(
-                    height: 200,
-                    width: 180,
+                    height: 30,
+                    width: 70,
                     decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/image_1.png"),
-                      ),
+                      color: mainColor,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "SAMANTHA",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey.withValues(alpha: 0.8),
-                          ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15.0, top: 4),
+                      child: Text(
+                        "more",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
                         ),
-                        Spacer(),
-                        Text("\$400",style: TextStyle(fontWeight: FontWeight.w600),),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      "Russia",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: mainColor.withValues(alpha: 0.5),
                       ),
                     ),
                   ),
                 ],
               ),
             ),
+
           ],
         ),
       ),
---
+++ lib/constants/tile.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class Tile extends StatelessWidget {
+  const Tile({super.key, required this.image, required this.Name, required this.Price, required this.country});
+  final String image;
+  final String Name;
+  final String Price;
+  final String country;
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 150,
+        width: 150,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [BoxShadow(
+              color: mainColor,
+              offset: Offset(0, 5),
+              blurRadius: 5
+          )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20)),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(image),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Row(
+                children: [
+                  Text(
+                    Name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.withValues(alpha: 0.8),
+                    ),
+                  ),
+                  Spacer(),
+                  Text(Price,style: TextStyle(fontWeight: FontWeight.w600),),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 4.0),
+              child: Text(
+                country,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: mainColor.withValues(alpha: 0.5),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
